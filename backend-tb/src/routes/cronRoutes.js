6a6ffaef8a8749d75d3a226df982fe3b69bb8e95
GEND1T <akunumum1606@gmail.com>
2026-06-07 10:51:15 +0700

    Update cron routes to remove '/cron' prefix

--- backend-tb/src/routes/cronRoutes.js
+++ backend-tb/src/routes/cronRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cronController = require('../controllers/cronController');
 
 // Tambahkan rute GET baru ini
-router.get('/cron/rekap-mingguan', cronController.triggerRekapMingguan);
-router.get('/cron/rekap-harian', cronController.triggerRekapHarian);
+router.get('/rekap-mingguan', cronController.triggerRekapMingguan);
+router.get('/rekap-harian', cronController.triggerRekapHarian);
 
 module.exports = router;
